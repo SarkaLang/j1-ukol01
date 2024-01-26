d09a9e293f6e73d1715e04a7e2548881c127590e
SarkaLang <sarka.langy@gmail.com>
2024-01-26 09:42:13 +0100

    Úkol 1 prasátko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -11,8 +11,51 @@ public class HlavniProgram {
 
   public void start() {
     zofka = new Turtle();
+    nakresliPrasatko();
+  }
 
-    //TODO implementace domácího úkolu
+  public void nozickyJedna() {
+    zofka.turnRight(70);
+    zofka.move(30);
+
+    zofka.turnLeft(180);
+    zofka.move(30);
+
+    zofka.turnRight(150);
+    zofka.move(30);
+
+    zofka.turnLeft(180);
+    zofka.move(30);
+  }
+
+  public void nozickyDva() {
+    zofka.turnRight(100);
+    zofka.move(30);
+
+    zofka.turnLeft(180);
+    zofka.move(30);
+
+    zofka.turnRight(150);
+    zofka.move(30);
+  }
+
+    public void nakresliPrasatko() {
+      for (int i = 0; i < 4; i++) {
+        zofka.turnRight(90);
+        zofka.move(100);
+      }
+
+      for (int i = 0; i < 2; i++) {
+        zofka.turnLeft(120);
+        zofka.move(100);
+      }
+
+      nozickyJedna();
+
+      zofka.turnRight(110);
+      zofka.move(100);
+
+      nozickyDva();
   }
 
 }
