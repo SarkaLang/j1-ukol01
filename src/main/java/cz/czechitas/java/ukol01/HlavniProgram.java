4babf370a5744af22f354819ac1e0f3645979754
SarkaLang <sarka.langy@gmail.com>
2024-01-27 11:36:13 +0100

    Domečky, prasátko + slučníko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -12,9 +12,7 @@ public class HlavniProgram {
   public void start() {
     zofka = new Turtle();
     nakresliPrasatko();
-    mezera();
-    slunicko();
-    slunickoDva();
+    nakresliDomecek();
   }
 
   public void nozickyJedna() {
@@ -43,16 +41,36 @@ public class HlavniProgram {
     zofka.move(30);
   }
 
-  public void nakresliPrasatko() {
+  public void ctverecek() {
     for (int i = 0; i < 4; i++) {
       zofka.turnRight(90);
       zofka.move(100);
     }
+  }
 
+  public void strecha() {
     for (int i = 0; i < 2; i++) {
       zofka.turnLeft(120);
       zofka.move(100);
     }
+  }
+
+  public void slunicko() {
+    for (int i = 0; i < 6; i++) {
+      zofka.move(15);
+      zofka.turnRight(30);
+      zofka.turnLeft(90);
+      zofka.move(10);
+      zofka.turnRight(180);
+      zofka.move(10);
+      zofka.turnLeft(45);
+      zofka.move(5);
+    }
+  }
+
+  public void nakresliPrasatko() {
+    ctverecek();
+    strecha();
 
     nozickyJedna();
 
@@ -60,16 +78,11 @@ public class HlavniProgram {
     zofka.move(100);
 
     nozickyDva();
+
+    mezeraVLevo();
   }
 
-  public void mezera() {
-    zofka.penUp();
-    zofka.turnLeft(120);
-    zofka.move(100);
-    zofka.penDown();
-  }
-
-  // zkouseka obrazcu
+    /* zkouseka obrazcu
   public void slunicko() {
     for (int i = 0; i < 20; i++) {
       zofka.move(5);
@@ -85,8 +98,76 @@ public class HlavniProgram {
       zofka.turnRight(30);
       zofka.move(10);
     }
+  }
+   */
+
+  // mezery
+  public void mezeraVLevo() {
+    zofka.penUp();
+    zofka.turnLeft(160);
+    zofka.turnLeft(90);
+    zofka.move(300);
+    zofka.turnRight(180);
+    zofka.penDown();
+  }
+
+  public void mezeraNahoru() {
+    zofka.penUp();
+    zofka.turnRight(150);
+    zofka.move(250);
+    zofka.turnRight(90);
+    zofka.move(80);
+    zofka.penDown();
+  }
+
+  public void mezeraMezeDomecky() {
+    zofka.turnLeft(120);
+    zofka.penUp();
+    zofka.move(250);
+    zofka.penDown();
+  }
+
+  public void mezeraDolu() {
+    zofka.penUp();
+    zofka.turnLeft(30);
+    zofka.move(250);
+    zofka.turnLeft(90);
+    zofka.move(50);
+    zofka.penDown();
+
+  }
+    // domecky
+    public void nakresliDomecek() {
+      ctverecek();
+      strecha();
+
+      mezeraNahoru();
+
+      ctverecek();
+      strecha();
+
+      for (int i = 0; i < 4; i++) {
+        mezeraMezeDomecky();
+        ctverecek();
+        strecha();
+      }
+
+      mezeraDolu();
+      ctverecek();
+      strecha();
+
+      zofka.penUp();
+      zofka.turnRight(100);
+      zofka.move(550);
+      zofka.penDown();
+
+      slunicko();
+
+      zofka.penUp();
+      zofka.turnRight(110);
+
+    }
+
 
   }
 
-
-}
